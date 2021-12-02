f320fbdebca34a04be35c75c30497d3431ab37ac
JayA96 <58231216+JayA96@users.noreply.github.com>
2021-12-02 16:10:52 +0000

    removed obsolete imports from icons.js

--- src/icons.js
+++ src/icons.js
@@ -1,249 +1,65 @@
-import uvindex11 from './assets/animated/uv-index-11.svg';
-import thermometerglassfahrenheit from './assets/animated/thermometer-glass-fahrenheit.svg';
 import clearnight from './assets/animated/clear-night.svg';
-import thunderstormsrain from './assets/animated/thunderstorms-rain.svg';
-import thermometerwarmer from './assets/animated/thermometer-warmer.svg';
 import wind from './assets/animated/wind.svg';
-import uvindex10 from './assets/animated/uv-index-10.svg';
-import moonwaningcrescent from './assets/animated/moon-waning-crescent.svg';
-import raindrop from './assets/animated/raindrop.svg';
-import sunrise from './assets/animated/sunrise.svg';
-import moonset from './assets/animated/moonset.svg';
 import snow from './assets/animated/snow.svg';
-import umbrella from './assets/animated/umbrella.svg';
 import thunderstormsday from './assets/animated/thunderstorms-day.svg';
 import dust from './assets/animated/dust.svg';
-import pressurelow from './assets/animated/pressure-low.svg';
-import thunderstormsnightsnow from './assets/animated/thunderstorms-night-snow.svg';
-import thunderstorms from './assets/animated/thunderstorms.svg';
-import dustday from './assets/animated/dust-day.svg';
 import partlycloudynightsleet from './assets/animated/partly-cloudy-night-sleet.svg';
-import moonwaxinggibbous from './assets/animated/moon-waxing-gibbous.svg';
-import partlycloudynightfog from './assets/animated/partly-cloudy-night-fog.svg';
 import tornado from './assets/animated/tornado.svg';
 import haze from './assets/animated/haze.svg';
-import horizon from './assets/animated/horizon.svg';
-import windbeaufort9 from './assets/animated/wind-beaufort-9.svg';
-import uvindex7 from './assets/animated/uv-index-7.svg';
 import hazenight from './assets/animated/haze-night.svg';
-import partlycloudydayhail from './assets/animated/partly-cloudy-day-hail.svg';
 import partlycloudynightsnow from './assets/animated/partly-cloudy-night-snow.svg';
 import partlycloudynight from './assets/animated/partly-cloudy-night.svg';
-import thermometercelsius from './assets/animated/thermometer-celsius.svg';
-import notavailable from './assets/animated/not-available.svg';
-import uvindex6 from './assets/animated/uv-index-6.svg';
-import hurricane from './assets/animated/hurricane.svg';
-import thermometermercury from './assets/animated/thermometer-mercury.svg';
-import windbeaufort8 from './assets/animated/wind-beaufort-8.svg';
-import thermometermercurycold from './assets/animated/thermometer-mercury-cold.svg';
-import uvindex4 from './assets/animated/uv-index-4.svg';
-import thunderstormsdaysnow from './assets/animated/thunderstorms-day-snow.svg';
-import pressurelowalt from './assets/animated/pressure-low-alt.svg';
-import thermometerglasscelsius from './assets/animated/thermometer-glass-celsius.svg';
-import uvindex5 from './assets/animated/uv-index-5.svg';
-import partlycloudynighthaze from './assets/animated/partly-cloudy-night-haze.svg';
 import fog from './assets/animated/fog.svg';
-import moonlastquarter from './assets/animated/moon-last-quarter.svg';
-import windbeaufort10 from './assets/animated/wind-beaufort-10.svg';
-import uvindex1 from './assets/animated/uv-index-1.svg';
-import smokeparticles from './assets/animated/smoke-particles.svg';
-import thermometercolder from './assets/animated/thermometer-colder.svg';
-import thermometer from './assets/animated/thermometer.svg';
-import partlycloudydayrain from './assets/animated/partly-cloudy-day-rain.svg';
-import snowflake from './assets/animated/snowflake.svg';
-import windbeaufort11 from './assets/animated/wind-beaufort-11.svg';
-import fogday from './assets/animated/fog-day.svg';
-import uvindex2 from './assets/animated/uv-index-2.svg';
-import partlycloudydayfog from './assets/animated/partly-cloudy-day-fog.svg';
 import sleet from './assets/animated/sleet.svg';
-import uvindex3 from './assets/animated/uv-index-3.svg';
-import partlycloudydaysmoke from './assets/animated/partly-cloudy-day-smoke.svg';
-import windbeaufort12 from './assets/animated/wind-beaufort-12.svg';
-import windbeaufort0 from './assets/animated/wind-beaufort-0.svg';
-import solareclipse from './assets/animated/solar-eclipse.svg';
 import clearday from './assets/animated/clear-day.svg';
-import starrynight from './assets/animated/starry-night.svg';
 import rain from './assets/animated/rain.svg';
 import drizzle from './assets/animated/drizzle.svg';
-import sunset from './assets/animated/sunset.svg';
-import fallingstars from './assets/animated/falling-stars.svg';
-import windbeaufort1 from './assets/animated/wind-beaufort-1.svg';
-import windbeaufort3 from './assets/animated/wind-beaufort-3.svg';
-import windsock from './assets/animated/windsock.svg';
-import overcast from './assets/animated/overcast.svg';
 import partlycloudynightsmoke from './assets/animated/partly-cloudy-night-smoke.svg';
-import thunderstormssnow from './assets/animated/thunderstorms-snow.svg';
-import moonrise from './assets/animated/moonrise.svg';
-import partlycloudydaydrizzle from './assets/animated/partly-cloudy-day-drizzle.svg';
 import mist from './assets/animated/mist.svg';
-import humidity from './assets/animated/humidity.svg';
-import windbeaufort2 from './assets/animated/wind-beaufort-2.svg';
-import windbeaufort6 from './assets/animated/wind-beaufort-6.svg';
-import hail from './assets/animated/hail.svg';
-import uvindex8 from './assets/animated/uv-index-8.svg';
-import moonnew from './assets/animated/moon-new.svg';
 import overcastday from './assets/animated/overcast-day.svg';
-import celsius from './assets/animated/celsius.svg';
 import partlycloudyday from './assets/animated/partly-cloudy-day.svg';
 import overcastnight from './assets/animated/overcast-night.svg';
-import star from './assets/animated/star.svg';
-import uvindex9 from './assets/animated/uv-index-9.svg';
-import windbeaufort7 from './assets/animated/wind-beaufort-7.svg';
-import windbeaufort5 from './assets/animated/wind-beaufort-5.svg';
 import fognight from './assets/animated/fog-night.svg';
 import thunderstormsnightrain from './assets/animated/thunderstorms-night-rain.svg';
-import moonwaxingcrescent from './assets/animated/moon-waxing-crescent.svg';
-import moonwaninggibbous from './assets/animated/moon-waning-gibbous.svg';
-import dustwind from './assets/animated/dust-wind.svg';
-import compass from './assets/animated/compass.svg';
-import hazeday from './assets/animated/haze-day.svg';
-import lightningbolt from './assets/animated/lightning-bolt.svg';
-import windbeaufort4 from './assets/animated/wind-beaufort-4.svg';
-import pressurehigh from './assets/animated/pressure-high.svg';
 import thunderstormsdayrain from './assets/animated/thunderstorms-day-rain.svg';
-import barometer from './assets/animated/barometer.svg';
-import partlycloudydaysleet from './assets/animated/partly-cloudy-day-sleet.svg';
 import smoke from './assets/animated/smoke.svg';
-import partlycloudydayhaze from './assets/animated/partly-cloudy-day-haze.svg';
 import partlycloudynightrain from './assets/animated/partly-cloudy-night-rain.svg';
-import moonfirstquarter from './assets/animated/moon-first-quarter.svg';
 import dustnight from './assets/animated/dust-night.svg';
-import pressurehighalt from './assets/animated/pressure-high-alt.svg';
-import thermometerfahrenheit from './assets/animated/thermometer-fahrenheit.svg';
 import cloudy from './assets/animated/cloudy.svg';
-import moonfull from './assets/animated/moon-full.svg';
-import fahrenheit from './assets/animated/fahrenheit.svg';
-import raindrops from './assets/animated/raindrops.svg';
 import partlycloudynightdrizzle from './assets/animated/partly-cloudy-night-drizzle.svg';
-import partlycloudynighthail from './assets/animated/partly-cloudy-night-hail.svg';
-import thermometerglass from './assets/animated/thermometer-glass.svg';
 import thunderstormsnight from './assets/animated/thunderstorms-night.svg';
-import partlycloudydaysnow from './assets/animated/partly-cloudy-day-snow.svg';
-import uvindex from './assets/animated/uv-index.svg';
 
 const Icons = {
-uvindex11,
-thermometerglassfahrenheit,
 clearnight,
-thunderstormsrain,
-thermometerwarmer,
 wind,
-uvindex10,
-moonwaningcrescent,
-raindrop,
-sunrise,
-moonset,
 snow,
-umbrella,
 thunderstormsday,
 dust,
-pressurelow,
-thunderstormsnightsnow,
-thunderstorms,
-dustday,
 partlycloudynightsleet,
-moonwaxinggibbous,
-partlycloudynightfog,
 tornado,
 haze,
-horizon,
-windbeaufort9,
-uvindex7,
 hazenight,
-partlycloudydayhail,
 partlycloudynightsnow,
 partlycloudynight,
-thermometercelsius,
-notavailable,
-uvindex6,
-hurricane,
-thermometermercury,
-windbeaufort8,
-thermometermercurycold,
-uvindex4,
-thunderstormsdaysnow,
-pressurelowalt,
-thermometerglasscelsius,
-uvindex5,
-partlycloudynighthaze,
 fog,
-moonlastquarter,
-windbeaufort10,
-uvindex1,
-smokeparticles,
-thermometercolder,
-thermometer,
-partlycloudydayrain,
-snowflake,
-windbeaufort11,
-fogday,
-uvindex2,
-partlycloudydayfog,
 sleet,
-uvindex3,
-partlycloudydaysmoke,
-windbeaufort12,
-windbeaufort0,
-solareclipse,
 clearday,
-starrynight,
 rain,
 drizzle,
-sunset,
-fallingstars,
-windbeaufort1,
-windbeaufort3,
-windsock,
-overcast,
 partlycloudynightsmoke,
-thunderstormssnow,
-moonrise,
-partlycloudydaydrizzle,
 mist,
-humidity,
-windbeaufort2,
-windbeaufort6,
-hail,
-uvindex8,
-moonnew,
 overcastday,
-celsius,
 partlycloudyday,
 overcastnight,
-star,
-uvindex9,
-windbeaufort7,
-windbeaufort5,
 fognight,
 thunderstormsnightrain,
-moonwaxingcrescent,
-moonwaninggibbous,
-dustwind,
-compass,
-hazeday,
-lightningbolt,
-windbeaufort4,
-pressurehigh,
 thunderstormsdayrain,
-barometer,
-partlycloudydaysleet,
 smoke,
-partlycloudydayhaze,
 partlycloudynightrain,
-moonfirstquarter,
 dustnight,
-pressurehighalt,
-thermometerfahrenheit,
 cloudy,
-moonfull,
-fahrenheit,
-raindrops,
 partlycloudynightdrizzle,
-partlycloudynighthail,
-thermometerglass,
 thunderstormsnight,
-partlycloudydaysnow,
-uvindex,
 };
 
 export default Icons;
